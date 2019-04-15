96686a846799e8525f9aad75d74863d0db1649ba
David Woodhouse <dwmw2@infradead.org>
2019-04-15 16:53:59 +0100

    Add AES-NI ESP implementation
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- openconnect-internal.h
+++ openconnect-internal.h
@@ -35,6 +35,10 @@
 
 #include "json.h"
 
+#ifdef AESNI_ASM
+#include "aesni-esp.h"
+#endif
+
 #if defined(OPENCONNECT_OPENSSL)
 #include <openssl/ssl.h>
 #include <openssl/err.h>
@@ -357,6 +361,11 @@ struct esp {
 #elif defined(OPENCONNECT_OPENSSL)
 	HMAC_CTX *hmac;
 	EVP_CIPHER_CTX *cipher;
+#endif
+#ifdef AESNI_ASM
+	struct aesni_hmac aesni_hmac;
+	struct aesni_key aesni_key;
+	unsigned char *aesni_hmac_block;
 #endif
 	uint64_t seq_backlog;
 	uint64_t seq;
