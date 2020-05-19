b5ba8468dc5ead218abe699c43c9f9098f288ef0
Daniel Lenski <dlenski@gmail.com>
2020-05-19 14:36:48 -0700

    add 'nullppp' protocol for testing
    Use `socat` to set up a PTY connected to a TLS socket (listening only on the local interface):
    
        socat -s -d -d \
          PTY,raw,b9600 \
          OPENSSL-LISTEN:5556,cert=tests/certs/server-cert.pem,key=tests/certs/server-key.pem,verify=0,so-bindtodevice=lo
        [ N PTY is /dev/pts/X ]
    
    Connect the PTY to `pppd` (requires root):
    
        # Add 'sync' to disable HDLC framing
        sudo pppd /dev/pts/X 10.0.0.1:10.0.0.101 noauth debug dump logfd 2 local nodetach passive persist ms-dns 1.1.1.1 ms-wins 5.5.5.5 +ipv6
    
    Connect OpenConnect to the TLS socket, and watch it negotiate LCP/IPCP/IP6CP with its peer, and reject CCP:
    
        # Add noipv4,noipv6 to cookie to try those
        ./openconnect --protocol=nullppp --cookie hdlc --servercert=d66b507ae074d03b02eafca40d35f87dd81049d3 --dump localhost:5556
    
    Signed-off-by: Daniel Lenski <dlenski@gmail.com>

---
+++ nullppp.c
@@ -0,0 +1,71 @@
+/*
+ * OpenConnect (SSL + DTLS) VPN client
+ *
+ * Copyright © 2020 David Woodhouse
+ *
+ * Author: David Woodhouse <dwmw2@infradead.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+#include <config.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdarg.h>
+#include <sys/types.h>
+
+#include "openconnect-internal.h"
+
+int nullppp_obtain_cookie(struct openconnect_info *vpninfo)
+{
+	if (!(vpninfo->cookie = strdup("")))
+		return -ENOMEM;
+	return 0;
+}
+
+int nullppp_connect(struct openconnect_info *vpninfo)
+{
+	int ret;
+	int ipv4, ipv6, hdlc;
+
+	/* XX: cookie hack. Use -C hdlc,noipv4,noipv6 on the
+	 * command line to set options. */
+	hdlc = strstr(vpninfo->cookie, "hdlc") ? 1 : 0;
+	ipv4 = strstr(vpninfo->cookie, "noipv4") ? 0 : 1;
+	ipv6 = strstr(vpninfo->cookie, "noipv6") ? 0 : 1;
+
+	/* Now establish the actual connection */
+	ret = openconnect_open_https(vpninfo);
+	if (ret)
+		goto out;
+
+	ret = openconnect_ppp_new(vpninfo,
+				  hdlc ? PPP_ENCAP_RFC1662_HDLC : PPP_ENCAP_RFC1661,
+				  ipv4, ipv6);
+
+ out:
+	if (ret)
+		openconnect_close_https(vpninfo, 0);
+	else {
+		monitor_fd_new(vpninfo, ssl);
+		monitor_read_fd(vpninfo, ssl);
+		monitor_except_fd(vpninfo, ssl);
+	}
+
+	return ret;
+}
