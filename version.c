008679098bacf17bcc84d6b49ecb7d7979ddc465
Daniel Lenski <dlenski@gmail.com>
2021-03-31 10:39:37 -0700

    centralize/simplify versioning in version.sh
    Let's not make the Makefile jump through so many hoops to generate Windows-ified version strings.
    
    Signed-off-by: Daniel Lenski <dlenski@gmail.com>

---
+++ version.c
@@ -0,0 +1,3 @@
+#include "version.h"
+
+const char *openconnect_version_str = OPENCONNECT_VERSION_STR;
--- version.sh
+++ version.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+config_h="$1"
+version_h="$2"
+version_nsi="$3"
+
 v="v8.10"
 
 if [ -d ${GIT_DIR:-.git} ] && tag=`git describe --tags`; then
@@ -17,5 +21,27 @@ else # XXX: Equivalent for .deb packages?
 	v="$v"-unknown
 fi
 
-echo "const char *openconnect_version_str = \"$v\";" > $1
+# Windows "product version" has to be a a.b.c.d (numeric)
+windows_quad_version=$(echo "$v" | perl -ne 'm|v(\d+)\.(\d+)(?:\.git\.\|\-)?(\d+)?(?:-g.+\|.*)|; print "$1.$2." . ($3 or "0") . ".0"');
+
+# TLS library name
+if test ! -e $config_h; then
+    exit 1
+elif egrep -q '^#define OPENCONNECT_GNUTLS' $config_h; then
+    TLS_LIBRARY=GnuTLS
+elif egrep -q '^#define OPENCONNECT_OPENSSL' $config_h; then
+    TLS_LIBRARY=OpenSSL
+else
+    TLS_LIBRARY="Unknown TLS library"
+fi
+
+cat <<EOF > $version_h
+#define OPENCONNECT_VERSION_STR "$v";
+#define OPENCONNECT_WINDOWS_QUAD_VERSION "$windows_quad_version";
+#define OPENCONNECT_TLS_LIBRARY "$TLS_LIBRARY";
+EOF
+
+sed 's|\#define|!define|; s|;\s*$||;' < $version_h > $version_nsi
+
 echo "New version: $v"
+echo "Windows dotted-quad version: $windows_quad_version"
