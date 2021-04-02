b3b271bfc5eefbd1c3c8ba893f54fa507fa258c5
Joachim Kuebart <joachim.kuebart@gmail.com>
2021-04-02 23:16:10 +0200

    add juniper-sso-auth test: add unit test for Azure MFA SSO
    Adds a new fake-juniper-sso-server.py, alongside fake-juniper-server.py
    (clearer this way, since they share very little in the way of auth handling).
    
    Signed-off-by: Joachim Kuebart <joachim.kuebart@gmail.com>

---
+++ tests/fake-tncc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+
+def main():
+    """
+    Reply "success" when we receive "hostchecker".
+    """
+    io = os.fdopen(0, "r+b", buffering=0)
+    started = False
+    for line in io:
+        line = line.decode("ascii").rstrip()
+        if line == "start":
+            started = True
+        if started and line == "Cookie=hostchecker":
+            io.write(b"200\n3\nsuccess\n\n\n")
+            started = False
+
+if __name__ == "__main__":
+    main()
