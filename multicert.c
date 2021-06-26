f8852a922f9b5ca64a414eb1bc911d752772d221
Tom Carroll <incentivedesign@gmail.com>
2021-06-26 13:49:35 -0700

    Updated multicert string maps to revised certificate API.
    Signed-off-by: Tom Carroll <incentivedesign@gmail.com>

--- multicert.c
+++ multicert.c
@@ -5,22 +5,15 @@
 #define NELEM(array)	(sizeof(array)/sizeof(array[0]))
 
 static const struct {
-	multicert_signhash_algorithm_t id;
+	openconnect_hash_type id;
 	const char *name;
-} digest_table[MULTICERT_SIGNHASH_MAX + 1] = {
-	[ MULTICERT_SIGNHASH_SHA256 ] = { MULTICERT_SIGNHASH_SHA256, "sha256" },
-	[ MULTICERT_SIGNHASH_SHA384 ] = { MULTICERT_SIGNHASH_SHA384, "sha384" },
-	[ MULTICERT_SIGNHASH_SHA512 ] = { MULTICERT_SIGNHASH_SHA512, "sha512" },
+} digest_table[OPENCONNECT_HASH_MAX + 1] = {
+	[ OPENCONNECT_HASH_SHA256 ] = { OPENCONNECT_HASH_SHA256, "sha256" },
+	[ OPENCONNECT_HASH_SHA384 ] = { OPENCONNECT_HASH_SHA384, "sha384" },
+	[ OPENCONNECT_HASH_SHA512 ] = { OPENCONNECT_HASH_SHA512, "sha512" },
 };
 
-static const struct {
-	multicert_cert_format_t id;
-	const char *name;
-} cert_format_table[MULTICERT_CERT_FORMAT_MAX + 1] = {
-	[ MULTICERT_CERT_FORMAT_PKCS7 ] = { MULTICERT_CERT_FORMAT_PKCS7, "pkcs7" },
-};
-
-const char *multicert_signhash_get_name(int id)
+const char *multicert_hash_get_name(int id)
 {
 	size_t i;
 
@@ -32,7 +25,7 @@ const char *multicert_signhash_get_name(int id)
 	return NULL;
 }
 
-multicert_signhash_algorithm_t multicert_signhash_get_id(const char *name)
+openconnect_hash_type multicert_hash_get_id(const char *name)
 {
 	size_t i;
 
@@ -43,31 +36,5 @@ multicert_signhash_algorithm_t multicert_signhash_get_id(const char *name)
 				return digest_table[i].id;
 		}
 	}
-	return MULTICERT_SIGNHASH_UNKNOWN;
-}
-
-const char *multicert_cert_format_get_name(int id)
-{
-	size_t i;
-
-	if (id > 0 && (size_t) id < NELEM(cert_format_table)) {
-		i = (size_t) id;
-		if (cert_format_table[i].id)
-			return cert_format_table[i].name;
-	}
-	return NULL;
-}
-
-multicert_cert_format_t multicert_cert_format_get_id(const char *name)
-{
-	size_t i;
-
-	if (name) {
-		for (i = 1; i < NELEM(cert_format_table); i++) {
-			const char *format_name = cert_format_table[i].name;
-			if (format_name && !strcmp(format_name, name))
-				return cert_format_table[i].id;
-		}
-	}
-	return MULTICERT_CERT_FORMAT_UNKNOWN;
+	return OPENCONNECT_HASH_UNKNOWN;
 }
