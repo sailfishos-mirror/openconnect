sailfishos-mirror/openconnect

commit b4f50f8bd5da7e6ac926ddd5095501edbc204cd0

For Pulse, send ESP only of the same IP protocol as we're connected over
It really seems that when we're connected over Legacy IP, it only accepts
Legacy IP packets in ESP. And when we're connected over IPv6, it only
accepts IPv6 packets in ESP.

This matches the behaviour of the Windows client too.

If you connect to a NC server over IPv6 it doesn't even offer the ESP
config (since NC doesn't support IPv6 within the tunnel).

Someone really ought to report this bug to Pulse. For IPv6 VPN traffic
to be forced into TCP-over-TCP mode when you happen to be connected to
the VPN over Legacy IP is very bad.

Signed-off-by: David Woodhouse <dwmw2@infradead.org>

esp.c
@@ -302,10 +302,27 @@ int esp_mainloop(struct openconnect_info *vpninfo, int *timeout, int readable)
 			this = vpninfo->deflate_pkt;
 			len = this->len;
 		} else {
+			uint8_t dontsend;
+
 			this = dequeue_packet(&vpninfo->outgoing_queue);
 			if (!this)
 				break;
 
+			/* Pulse can only accept ESP of the same protocol as the one you
+			 * connected to it with. The other has to go over IF-T/TLS. */
+			if (vpninfo->dtls_addr->sa_family == AF_INET6)
+				dontsend = 0x40;
+			else
+				dontsend = 0x60;
+
+			if ( (this->data[0] & 0xf0) == dontsend) {
+				store_be32(&this->pulse.vendor, 0xa4c);
+				store_be32(&this->pulse.type, 4);
+				store_be32(&this->pulse.len, this->len + 16);
+				queue_packet(&vpninfo->oncp_control_queue, this);
+				work_done = 1;
+				continue;
+			}
 			len = construct_esp_packet(vpninfo, this, 0);
 			if (len < 0) {
 				/* Should we disable ESP? */

mainloop.c
@@ -79,8 +79,8 @@ int tun_mainloop(struct openconnect_info *vpninfo, int *timeout, int readable)
 			vpninfo->stats.tx_bytes += out_pkt->len;
 			work_done = 1;
 
-			if (queue_packet(&vpninfo->outgoing_queue, out_pkt) ==
-			    vpninfo->max_qlen) {
+			if (queue_packet(&vpninfo->outgoing_queue, out_pkt) +
+			    vpninfo->oncp_control_queue.count >= vpninfo->max_qlen) {
 				out_pkt = NULL;
 				unmonitor_read_fd(vpninfo, tun);
 				break;
@@ -88,7 +88,7 @@ int tun_mainloop(struct openconnect_info *vpninfo, int *timeout, int readable)
 			out_pkt = NULL;
 		}
 		vpninfo->tun_pkt = out_pkt;
-	} else if (vpninfo->outgoing_queue.count < vpninfo->max_qlen) {
+	} else if (vpninfo->outgoing_queue.count + vpninfo->oncp_control_queue.count < vpninfo->max_qlen) {
 		monitor_read_fd(vpninfo, tun);
 	}
 

oncp.c
@@ -1305,7 +1305,8 @@ int oncp_esp_send_probes(struct openconnect_info *vpninfo)
 	for (seq=1; seq <= (vpninfo->dtls_state==DTLS_CONNECTED ? 1 : 2); seq++) {
 		pkt->len = 1;
 		pkt->data[0] = 0;
-		pktlen = construct_esp_packet(vpninfo, pkt, IPPROTO_IPIP);
+		pktlen = construct_esp_packet(vpninfo, pkt,
+					      vpninfo->dtls_addr->sa_family == AF_INET6 ? IPPROTO_IPV6 : IPPROTO_IPIP);
 		if (pktlen >= 0)
 			send(vpninfo->dtls_fd, (void *)&pkt->esp, pktlen, 0);
 	}

commit 3d1ec6e0a126d4b9fdd18473558cf816d2045b76

Juniper/NC ESP rekey fix
This should fix https://gitlab.com/openconnect/openconnect/-/issues/322.
This was an immensely tricky issue to identify and solve.  Many thanks to
@john508 for his patient logging, testing, and bisecting.

This came down to a subtle combination of 2 problems, described further in
https://gitlab.com/openconnect/openconnect/-/issues/322#note_702122197.

First, a bug:

  The code for Juniper/Pulse 'dontsend' added in
  https://gitlab.com/openconnect/openconnect/-/commit/b4f50f8bd5da7e6ac926ddd5095501edbc204cd0
  caused IPv6 packets to get sent over the Juniper oNCP/TLS channel in a
  completely mangled form, thereby confusing the server and making it not
  respond to subsequent ESP rekey packets sent over the oNCP/TLS channel.

  The fix is to use this code path *only* for the Pulse protocol, not for the
  Juniper protocol. Juniper cannot do ESP-over-IPv6 at all, and cannot send
  tunneled IPv6 packets at all (neither via ESP-over-IPv4, nor via oNCP/TLS).

Second, we introduced a subtle regression against a Juniper server behavior
which we weren't previously aware of:

  Juniper servers apparently don't always resend the IP address in a
  configuration packet, if that configuration packet is for
  rekey/reconnection only.

  To address this, we need to add a special case for Juniper to the
  install_vpn_opts() function, which was added in
  https://gitlab.com/openconnect/openconnect/-/commit/3d845bc9bf62b3e816e0d2fd72970ef33964a191.

Signed-off-by: Daniel Lenski <dlenski@gmail.com>

esp.c
@@ -315,11 +315,10 @@ int esp_mainloop(struct openconnect_info *vpninfo, int *timeout, int readable)
 				break;
 			ip_version = this->data[0] >> 4;
 
-			if (vpninfo->proto->proto == PROTO_NC ||
-			    vpninfo->proto->proto == PROTO_PULSE) {
+			if (vpninfo->proto->proto == PROTO_PULSE) {
 				uint8_t dontsend;
 
-				/* Pulse/NC can only accept ESP of the same protocol as the one
+				/* Pulse can only accept ESP of the same protocol as the one
 				 * you connected to it with. The other has to go over IF-T/TLS. */
 				if (vpninfo->dtls_addr->sa_family == AF_INET6)
 					dontsend = 4;
@@ -334,6 +333,15 @@ int esp_mainloop(struct openconnect_info *vpninfo, int *timeout, int readable)
 					work_done = 1;
 					continue;
 				}
+			} else if (vpninfo->proto->proto == PROTO_NC &&
+				   vpninfo->dtls_addr->sa_family == AF_INET6) {
+				/* Juniper/NC cannot do ESP-over-IPv6, and it cannot send
+				 * tunneled IPv6 packets at all; they just get dropped.
+				 * It shouldn't even send any ESP options/keys when connecting
+				 * to the server via IPv6. This should never happen.
+				 */
+				vpninfo->quit_reason = "Juniper/NC ESP tunnel over IPv6 should never happen.";
+				return 1;
 			}
 
 			/* XX: Must precede in-place encryption of the packet, because

library.c
@@ -476,13 +476,29 @@ void free_optlist(struct oc_vpn_option *opt)
 int install_vpn_opts(struct openconnect_info *vpninfo, struct oc_vpn_option *opt,
 		     struct oc_ip_info *ip_info)
 {
-	/* F5 doesn't get its IP address until it actually establishes the
-	 * PPP connection. */
-	if (vpninfo->proto->proto != PROTO_F5 && !ip_info->addr &&
-	    !ip_info->addr6 && !ip_info->netmask6) {
-		vpn_progress(vpninfo, PRG_ERR,
-			     _("No IP address received. Aborting\n"));
-		return -EINVAL;
+	/* XX: remove protocol-specific exceptions here, once we can test them
+	 * with F5 reconnections in addition to Juniper reconnections. See:
+	 * https://gitlab.com/openconnect/openconnect/-/merge_requests/293#note_702388182
+	 */
+	if (!ip_info->addr && !ip_info->addr6 && !ip_info->netmask6) {
+		if (vpninfo->proto->proto == PROTO_F5) {
+			/* F5 doesn't get its IP address until it actually establishes the
+			 * PPP connection. */
+		} else if (vpninfo->proto->proto == PROTO_NC && vpninfo->ip_info.addr) {
+			/* Juniper doesn't necessarily resend the Legacy IP address in the
+			 * event of a rekey/reconnection. */
+			ip_info->addr = add_option_dup(&opt, "ipaddr", vpninfo->ip_info.addr, -1);
+			if (!ip_info->netmask && vpninfo->ip_info.netmask)
+				ip_info->netmask = add_option_dup(&opt, "netmask", vpninfo->ip_info.netmask, -1);
+			vpn_progress(vpninfo, PRG_DEBUG,
+				     _("No IP address received with Juniper rekey/reconnection.\n"));
+			goto after_ip_checks;
+		} else {
+			/* For all other protocols, not receiving any IP address is an error */
+			vpn_progress(vpninfo, PRG_ERR,
+				     _("No IP address received. Aborting\n"));
+			return -EINVAL;
+		}
 	}
 
 	if (vpninfo->ip_info.addr) {
@@ -519,6 +535,7 @@ int install_vpn_opts(struct openconnect_info *vpninfo, struct oc_vpn_option *opt
 		}
 	}
 
+ after_ip_checks:
 	/* Preserve gateway_addr and MTU if they were set */
 	ip_info->gateway_addr = vpninfo->ip_info.gateway_addr;
 	if (!ip_info->mtu)