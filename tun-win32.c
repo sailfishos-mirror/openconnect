f180621f10f23159b11e31e7aba02af0f64e4c49
Dimitri Papadopoulos <3350651-DimitriPapadopoulos@users.noreply.gitlab.com>
2022-11-03 23:14:45 +0100

    Case-insensitive TAP component ID
    In the exact same way we fixed support for Wintun 0.14.1.
    
    Signed-off-by: Dimitri Papadopoulos <3350651-DimitriPapadopoulos@users.noreply.gitlab.com>

--- tun-win32.c
+++ tun-win32.c
@@ -114,7 +114,7 @@ static intptr_t search_taps(struct openconnect_info *vpninfo, tap_callback *cb)
 			RegCloseKey(hkey);
 			continue;
 		}
-		if (!strcmp(buf, TAP_COMPONENT_ID) || !strcmp(buf, "root\\" TAP_COMPONENT_ID))
+		if (!stricmp(buf, TAP_COMPONENT_ID) || !stricmp(buf, "root\\" TAP_COMPONENT_ID))
 			adapter_type = ADAPTER_TUNTAP;
 		else if (!strcmp(buf, "wintun"))
 			adapter_type = ADAPTER_WINTUN;
