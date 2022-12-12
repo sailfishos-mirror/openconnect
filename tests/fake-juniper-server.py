57160c9f2673adbbe468db137b28da4187549061
Daniel Lenski <dlenski@gmail.com>
2022-12-11 16:06:39 -0800

    Persistent configuration for fake Juniper server
    1. Rework fake Juniper server to have a persistent configuration
    2. Factor out some of the most repetitive elements of juniper-auth tests
    
    This was already done for the fake Fortinet, F5, and GP servers.
    
    Signed-off-by: Daniel Lenski <dlenski@gmail.com>

--- tests/fake-juniper-server.py
+++ tests/fake-juniper-server.py
@@ -28,6 +28,7 @@ import base64
 from json import dumps
 from functools import wraps
 from flask import Flask, request, abort, redirect, url_for, make_response, session
+from dataclasses import dataclass
 
 host, port, *cert_and_maybe_keyfile = sys.argv[1:]
 
@@ -69,24 +70,43 @@ def check_form_against_session(*fields, use_query=False):
 
 ########################################
 
-# Respond to initial 'GET /' with a redirect to '/dana-na/auth/url_default/welcome.cgi'
-# [Save in the session for use later:
-#   list of realms,
-#   session confirmation requirement,
-#   token/2FA form name (can be frmLogin, for 2-password-in-one-form option)]
+# Configure the fake server. These settings will persist unless/until reconfigured or restarted:
+#   realms: Comma-separated list of realms to offer (frmLogin will contain a dropdown for this)
+#   roles: Comma-separated list of roles to offer (frmSelectRoles will contain links for each)
+#     --authgroup can fill EITHER realm OR role; we've never seen a VPN that uses both
+#   confirm: If True (default False), frmConfirmation will be added as the final step in auth
+#   token_form: If specified, name of token/2FA form to include
+#     (frmLogin, frmTotpToken, frmDefender, or frmNextToken)
+@dataclass
+class TestConfiguration:
+    realms: list = ()
+    roles: list = ()
+    confirm: bool = False
+    token_form: str = None
+C = TestConfiguration()
+
+
+@app.route('/CONFIGURE', methods=('POST', 'GET'))
+def configure():
+    global C
+    if request.method == 'POST':
+        C = TestConfiguration(
+            realms=request.form['realms'].split(',') if 'realms' in request.form else (),
+            roles=request.form['roles'].split(',') if 'roles' in request.form else (),
+            confirm=bool(request.form.get('confirm')),
+            token_form=request.form.get('token_form'))
+        return '', 201
+    else:
+        return 'Current configuration of fake Juniper server:\n{}\n'.format(C)
+
+
 @app.route('/')
 def root():
     # We don't support the Junos/Pulse protocol (which starts with this request)
     if request.headers.get('Upgrade') == 'IF-T/TLS 1.0' and request.headers.get('Content-Type') == 'EAP':
         return abort(501)
 
-    realms = request.args.get('realms')
-    roles = request.args.get('roles')
-    confirm = bool(request.args.get('confirm'))
-    token_form = request.args.get('token_form')
-    session.update(step='initial-GET', realms=realms and realms.split(','),
-                   roles=roles and roles.split(','),
-                   confirm=confirm, token_form=token_form)
+    session.update(step='initial-GET')
     # print(session)
     return redirect(url_for('frmLogin'))
 
@@ -94,14 +114,13 @@ def root():
 # frmLogin
 @app.route('/dana-na/auth/url_default/welcome.cgi')
 def frmLogin():
+    global C
     session.update(step='GET-frmLogin')
-    realms = session.get('realms')
-    token_form = session.get('token_form')
     sel = token = ''
-    if realms:
+    if C.realms:
         sel = '<select name="realm">%s</select>' % ''.join(
-            '<option value="%d">%s</option>' % nv for nv in enumerate(realms))
-    if token_form == 'frmLogin':
+            '<option value="%d">%s</option>' % nv for nv in enumerate(C.realms))
+    if C.token_form == 'frmLogin':
         token = '<input type="password" name="token_as_second_password"/>'
 
     return '''
@@ -117,12 +136,9 @@ def frmLogin():
 # depending on session['confirm']
 @app.route('/dana-na/auth/url_default/login.cgi', methods=['POST'])
 def frmLogin_post():
-    realms = session.get('realms')
-    confirm = session.get('confirm')
-    token_form = session.get('token_form')
-    roles = session.get('roles')
-    if realms:
-        assert 0 <= int(request.form.get('realm', -1)) < len(realms)
+    global C
+    if C.realms:
+        assert 0 <= int(request.form.get('realm', -1)) < len(C.realms)
     session.update(step='POST-login', username=request.form.get('username'),
                    password=request.form.get('password'),
                    realm=request.form.get('realm'))
@@ -130,10 +146,10 @@ def frmLogin_post():
 
     need_confirm = need_token = False
     got_confirm = got_token = None
-    if confirm:
+    if C.confirm:
         need_confirm = request.form.get('btnContinue') is None
         got_confirm = not need_confirm
-    if token_form:
+    if C.token_form:
         need_token = request.form.get('btnAction') is None and request.form.get('totpactionEnter') is None and request.form.get('token_as_second_password') is None
         got_token = not need_token
     session.update(got_token=got_token, got_confirm=got_confirm)
@@ -142,7 +158,7 @@ def frmLogin_post():
         return redirect(url_for('frm2FA'))
     elif need_confirm:
         return redirect(url_for('frmConfirmation'))
-    elif roles:
+    elif C.roles:
         return redirect(url_for('frmSelectRoles'))
     else:
         resp = redirect(url_for('webtop'))
@@ -154,10 +170,10 @@ def frmLogin_post():
 # This is some insane post-login realm-ish select-y thing
 @app.route('/dana-na/auth/url_default/select_role.cgi')
 def frmSelectRoles():
+    global C
     session.update(step='GET-frmSelectRoles')
-    roles = session.get('roles')
     dest = url_for('frmSelectRoles_AFTER')
-    roles = '\n'.join('<tr><td><a href="%s?role=%d">%s</a></td></tr>' % (dest, nn, role) for (nn, role) in enumerate(roles))
+    roles = '\n'.join('<tr><td><a href="%s?role=%d">%s</a></td></tr>' % (dest, nn, role) for (nn, role) in enumerate(C.roles))
     return '''
 <html><body><form name="frmSelectRoles">
 <table id="TABLE_SelectRole_1">
@@ -171,9 +187,9 @@ def frmSelectRoles():
 # Note the URL is shared with the frmLogin POST URL... so weird
 @app.route('/dana-na/auth/url_default/login.cgi', methods=['GET'])
 def frmSelectRoles_AFTER():
-    roles = session.get('roles')
-    assert roles
-    assert 0 <= int(request.args.get('role', -1)) < len(roles)
+    global C
+    assert C.roles
+    assert 0 <= int(request.args.get('role', -1)) < len(C.roles)
     session.update(step='AFTER-frmSelectRoles', role=request.form.get('role'))
     resp = redirect(url_for('webtop'))
     resp.set_cookie('DSID', cookify(dict(session)))
@@ -184,9 +200,9 @@ def frmSelectRoles_AFTER():
 # This redirects back to frmLogin_POST
 @app.route('/dana-na/auth/url_default/token.cgi')
 def frm2FA():
-    token_form = session.get('token_form')
-    submit_button = ('totpactionEnter' if token_form == 'frmTotpToken' else 'btnAction')
-    session.update(step='GET-frmConfirm')
+    global C
+    submit_button = ('totpactionEnter' if C.token_form == 'frmTotpToken' else 'btnAction')
+    session.update(step='GET-' + C.token_form)
     return '''
 <html><body><form name="%s" method="post" action="%s">
 Enter your 2FA token code.
@@ -195,7 +211,7 @@ Enter your 2FA token code.
 <input type="hidden" name="realm" value="%s"/>
 <input type="submit" name="%s" value="Sign In"/>
 </form></body></html>''' % (
-    token_form,
+    C.token_form,
     url_for('frmLogin_post'), session.get('username'), session.get('realm'),
     submit_button
 )
