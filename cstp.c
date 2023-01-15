4b444a226fcf6a6e3cd6a6377884a5c97dc0bcd8
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2023-01-15 14:08:23 -0800

    cstp: Check if cookies is NULL in sso_detect_done
    Before change if SSO implementers pass a NULL cookies array in
    oc_webview_result, cstp_sso_detect_done would blindly dereference the NULL
    pointer. This leads to panics that the implementer has to then trace inside of
    openconnect. This NULL pointer check is similar to the one in
    gpst_sso_detect_done for the headers array.
    
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

--- cstp.c
+++ cstp.c
@@ -1305,7 +1305,7 @@ int cstp_sso_detect_done(struct openconnect_info *vpninfo,
 	int i;
 
 	/* Note that, at least with some backends (eg: Google's), empty cookies might be set */
-	for (i=0; result->cookies[i] != NULL; i+=2) {
+	for (i=0; result->cookies != NULL && result->cookies[i] != NULL; i+=2) {
 		const char *cname = result->cookies[i], *cval = result->cookies[i+1];
 		if (!strcmp(vpninfo->sso_token_cookie, cname) && cval && cval[0] != '\0') {
 			vpninfo->sso_cookie_value = strdup(cval);
