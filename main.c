sailfishos-mirror/openconnect

commit a2fd6f4f2e8ac78e1ba28fd10fcbdf0f9074274f

New option to define server name in config file
Signed-off-by: Dimitri Papadopoulos <3350651+DimitriPapadopoulos@users.noreply.gitlab.com>

main.c
@@ -208,6 +208,7 @@ enum {
 	OPT_PROTOCOL,
 	OPT_PASSTOS,
 	OPT_VERSION,
+	OPT_SERVER,
 };
 
 #ifdef __sun__
@@ -300,6 +301,7 @@ static const struct option long_options[] = {
 #elif defined(OPENCONNECT_OPENSSL)
 	OPTION("openssl-ciphers", 1, OPT_CIPHERSUITES),
 #endif
+	OPTION("server", 1, OPT_SERVER),
 	OPTION(NULL, 0, 0)
 };
 
@@ -881,6 +883,7 @@ static void usage(void)
 	printf("      --cafile=FILE               %s\n", _("Cert file for server verification"));
 
 	printf("\n%s:\n", _("Internet connectivity"));
+	printf("      --server=SERVER             %s\n", _("Set VPN server"));
 	printf("  -P, --proxy=URL                 %s\n", _("Set proxy server"));
 	printf("      --proxy-auth=METHODS        %s\n", _("Set proxy authentication methods"));
 	printf("      --no-proxy                  %s\n", _("Disable proxy"));
@@ -1364,6 +1367,10 @@ static int autocomplete(int argc, char **argv)
 					autocomplete_special("FILENAME", comp_opt, prefixlen, NULL);
 					break;
 				}
+				break;
+
+			case OPT_SERVER: /* --server */
+				autocomplete_special("HOSTNAME", comp_opt, prefixlen, NULL);
 				break;
 
 			case 'i': /* --interface */
@@ -2025,6 +2032,10 @@ int main(int argc, char **argv)
 
 			vpninfo->ciphersuite_config = dup_config_arg();
 			break;
+		case OPT_SERVER:
+			if (openconnect_parse_url(vpninfo, config_arg))
+				exit(1);
+			break;
 		default:
 			usage();
 		}
@@ -2033,10 +2044,10 @@ int main(int argc, char **argv)
 	if (gai_overrides)
 		openconnect_override_getaddrinfo(vpninfo, gai_override_cb);
 
-	if (optind < argc - 1) {
+	if (optind < argc - (vpninfo->hostname ? 0 : 1)) {
 		fprintf(stderr, _("Too many arguments on command line\n"));
 		usage();
-	} else if (optind > argc - 1) {
+	} else if (optind > argc - (vpninfo->hostname ? 0 : 1)) {
 		fprintf(stderr, _("No server specified\n"));
 		usage();
 	}

openconnect.8.in
@@ -70,7 +70,7 @@ openconnect \- Multi-protocol VPN client, for Cisco AnyConnect VPNs and others
 .OP \-\-version\-string string
 .OP \-\-local-hostname string
 .OP \-\-os string
-.B [https://]\fIserver\fB[:\fIport\fB][/\fIgroup\fB]
+.B [\-\-server] [https://]\fIhost\fB[:\fIport\fB][/\fIgroup\fB]
 .YS
 
 .SH DESCRIPTION
@@ -345,6 +345,19 @@ Pass traffic to 'script' program over a UNIX socket, instead of to a kernel
 tun/tap device. This allows the VPN IP traffic to be handled entirely in
 userspace, for example by a program which uses lwIP to provide SOCKS access
 into the VPN.
+.TP
+.B \-\-server=[https://]\fIHOST\fB[:\fIPORT\fB][/\fIGROUP\fB]
+Define the VPN server as a simple
+.I HOST
+or as an URL containing the
+. I HOST
+and optionally the
+.I PORT
+number and the login 
+.I GROUP
+or realm.
+
+As an alternative, define the VPN server as non-option command line argument.
 .TP
 .B \-u,\-\-user=NAME
 Set login username to

commit 89ba7992211ddd43bfbb28e22d27e7aab950bfaf

Fix --server vs. positional argument handling
If we have a --server argument, we shouldn't expect to find a positional
argument at argv[optind]. We mostly got this right, except that we still
called config_lookup_host() with argv[optind] even when --server was
given.

The only thing that saved us from dying with a strcmp() on NULL was
the fact that the loop over the XML elements is using the fact that
vpninfo->hostname gets set as its terminating condition, so it never
got that far.

Fix that, because it's icky. And make the --server argument work for
XML config lookups too.

Fixes: a2fd6f4f2e8a ("New option to define server name in config file")
Signed-off-by: David Woodhouse <dwmw2@infradead.org>

main.c
@@ -1723,6 +1723,7 @@ int main(int argc, char **argv)
 	int opt;
 	char *config_arg;
 	char *config_filename;
+	const char *server_url = NULL;
 	char *token_str = NULL;
 	oc_token_mode_t token_mode = OC_TOKEN_MODE_NONE;
 	int reconnect_timeout = 300;
@@ -2225,8 +2226,7 @@ int main(int argc, char **argv)
 			vpninfo->certinfo[1].password = dup_config_arg();
 			break;
 		case OPT_SERVER:
-			if (openconnect_parse_url(vpninfo, config_arg))
-				exit(1);
+			server_url = keep_config_arg();
 			break;
 		default:
 			usage();
@@ -2236,11 +2236,15 @@ int main(int argc, char **argv)
 	if (gai_overrides)
 		openconnect_override_getaddrinfo(vpninfo, gai_override_cb);
 
-	if (optind < argc - (vpninfo->hostname ? 0 : 1)) {
+	if (!server_url) {
+		if (optind >= argc) {
+			fprintf(stderr, _("No server specified\n"));
+			usage();
+		}
+		server_url = argv[optind++];
+	}
+	if (optind < argc) {
 		fprintf(stderr, _("Too many arguments on command line\n"));
-		usage();
-	} else if (optind > argc - (vpninfo->hostname ? 0 : 1)) {
-		fprintf(stderr, _("No server specified\n"));
 		usage();
 	}
 
@@ -2306,11 +2310,12 @@ int main(int argc, char **argv)
 	if (vpninfo->certinfo[0].key && do_passphrase_from_fsid)
 		openconnect_passphrase_from_fsid(vpninfo);
 
-	if (config_lookup_host(vpninfo, argv[optind]))
+	if (config_lookup_host(vpninfo, server_url))
 		exit(1);
 
+	/* If config_lookup_host() didn't set it, it'd better be a URL */
 	if (!vpninfo->hostname) {
-		char *url = strdup(argv[optind]);
+		char *url = strdup(server_url);
 
 		if (openconnect_parse_url(vpninfo, url))
 			exit(1);