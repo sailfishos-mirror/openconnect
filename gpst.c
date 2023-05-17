sailfishos-mirror/openconnect

commit e2bbc2a1fb025e46326d19c433a8347cbdb700fe

Combine Legacy IP and IPv6 cases in GP config XML parsing
This simplifies the code structure a bit, with a goal of avoiding bugs like
that described in the parent commit ffa540088f36e273d8ecd31a6f4f4a7384e3ad96
('Prioritize IPv6 for GlobalProtect ESP "magic ping"').

Signed-off-by: Daniel Lenski <dlenski@gmail.com>

gpst.c
@@ -359,9 +359,11 @@ static int gpst_parse_config_xml(struct openconnect_info *vpninfo, xmlNode *xml_
 	int ii;
 
 #ifdef HAVE_ESP
-	int esp_keys = 0, esp_v4 = 0, esp_v6 = 0;
-	uint32_t esp_magic;
-	struct in6_addr esp6_magic;
+	int esp_keys = 0, esp_magic_af = 0;
+	union {
+		struct in6_addr v6;
+		struct in_addr v4;
+	} esp_magic;
 #endif /* HAVE_ESP */
 
 	if (!xml_node || !xmlnode_is_named(xml_node, "response"))
@@ -409,7 +411,8 @@ static int gpst_parse_config_xml(struct openconnect_info *vpninfo, xmlNode *xml_
 			vpninfo->ssl_times.last_rekey = time(NULL);
 			vpninfo->ssl_times.rekey = sec - 60;
 			vpninfo->ssl_times.rekey_method = REKEY_TUNNEL;
-		} else if (!xmlnode_get_val(xml_node, "gw-address", &s)) {
+		} else if (!xmlnode_get_val(xml_node, "gw-address", &s) ||
+			   !xmlnode_get_val(xml_node, "gw-address-v6", &s)) {
 			/* As remarked in oncp.c, "this is a tunnel; having a
 			 * gateway is meaningless." See esp_send_probes_gp for the
 			 * gory details of what this field actually means.
@@ -418,26 +421,18 @@ static int gpst_parse_config_xml(struct openconnect_info *vpninfo, xmlNode *xml_
 			 * value in order for both AFs to work over the tunnel (see 5b98b628
 			 * "GlobalProtect IPv6 ESP support").
 			 */
-			if (vpninfo->peer_addr->sa_family == IPPROTO_IP &&
+			int legacy = xmlnode_is_named(xml_node, "gw-address");
+			if (vpninfo->peer_addr->sa_family == (legacy ? IPPROTO_IP : IPPROTO_IPV6) &&
 			    vpninfo->ip_info.gateway_addr && strcmp(s, vpninfo->ip_info.gateway_addr))
 				vpn_progress(vpninfo, PRG_DEBUG,
 					     _("Gateway address in config XML (%s) differs from external gateway address (%s).\n"), s, vpninfo->ip_info.gateway_addr);
 #ifdef HAVE_ESP
-			if (!esp_v6) {
+			if (esp_magic_af == AF_INET6 && legacy) {
 				/* We ignore the Legacy IP tag if we've already gotten the IPv6 tag. */
-				esp_magic = inet_addr(s);
-				esp_v4 = 1;
+				esp_magic_af = legacy ? AF_INET : AF_INET6;
+				inet_pton(esp_magic_af, s, &esp_magic);
+#endif /* HAVE_ESP */
 			}
-#endif /* HAVE_ESP */
-		} else if (!xmlnode_get_val(xml_node, "gw-address-v6", &s)) {
-			if (vpninfo->peer_addr->sa_family == IPPROTO_IPV6 &&
-			    vpninfo->ip_info.gateway_addr && strcmp(s, vpninfo->ip_info.gateway_addr))
-				vpn_progress(vpninfo, PRG_DEBUG,
-					     _("IPv6 gateway address in config XML (%s) differs from external gateway address (%s).\n"), s, vpninfo->ip_info.gateway_addr);
-#ifdef HAVE_ESP
-			inet_pton(AF_INET6, s, &esp6_magic);
-			esp_v6 = 1;
-#endif /* HAVE_ESP */
 		} else if (!xmlnode_get_val(xml_node, "connected-gw-ip", &s)) {
 			if (vpninfo->ip_info.gateway_addr && strcmp(s, vpninfo->ip_info.gateway_addr))
 				vpn_progress(vpninfo, PRG_DEBUG, _("Config XML <connected-gw-ip> address (%s) differs from external\n"
@@ -552,12 +547,13 @@ static int gpst_parse_config_xml(struct openconnect_info *vpninfo, xmlNode *xml_
 			     _("GlobalProtect IPv6 support is experimental. Please report results to <%s>.\n"),
 			     "openconnect-devel@lists.infradead.org");
 #ifdef HAVE_ESP
-	if (esp_keys && esp_v6 && new_ip_info.addr6) {
-		/* We got ESP keys, an IPv6 esp_magic address, and an IPv6 address */
-		vpninfo->esp_magic_af = AF_INET6;
-		memcpy(vpninfo->esp_magic, &esp6_magic, sizeof(esp6_magic));
+	if (esp_keys &&
+	    ((esp_magic_af == AF_INET6 && new_ip_info.addr6) ||
+	     (esp_magic_af == AF_INET && new_ip_info.addr))) {
+		/* We got ESP keys, an IPvX esp_magic address, and an IPvX address */
+		vpninfo->esp_magic_af = esp_magic_af;
+		memcpy(vpninfo->esp_magic, &esp_magic, sizeof(esp_magic));
 
-	setup_esp_keys:
 		if (openconnect_setup_esp_keys(vpninfo, 0)) {
 			vpn_progress(vpninfo, PRG_ERR, "Failed to setup ESP keys.\n");
 		} else {
@@ -565,11 +561,6 @@ static int gpst_parse_config_xml(struct openconnect_info *vpninfo, xmlNode *xml_
 			vpninfo->dtls_times.last_rekey = time(&vpninfo->new_dtls_started);
 			vpninfo->delay_tunnel_reason = "awaiting GPST ESP connection";
 		}
-	} else if (esp_keys && esp_v4 && new_ip_info.addr) {
-		/* We got ESP keys, an IPv4 esp_magic address, and an IPv4 address */
-		vpninfo->esp_magic_af = AF_INET;
-		memcpy(vpninfo->esp_magic, &esp_magic, sizeof(esp_magic));
-		goto setup_esp_keys;
 	} else if (vpninfo->dtls_state != DTLS_DISABLED)
 		vpn_progress(vpninfo, PRG_ERR,
 			     _("Did not receive ESP keys and matching gateway in GlobalProtect config; tunnel will be TLS only.\n"));

commit 5d91a598ea7b42b0cf81d58ec5c8fb51b6b83178

Fix broken ESP config parsing for GlobalProtect
This was broken in
https://gitlab.com/openconnect/openconnect/-/commit/e2bbc2a1f#efecf80fa476ca5abf1502940e60d7984c6d1df9_426_430

As the comment below that change notes, "We ignore the Legacy IP tag
(<gw-address>) if we've already gotten the IPv6 (<gw-address-v6) tag."
We do indeed want to prioritize ESP-over-UDP-over-IPv6 over
ESP-over-UDP-over-IPv4.

However, this change broke things by making it so that effectively, "We
ignore either tag, unless we've already received a tag."

Thanks to nemo44@gmail.com for bringing this to our attention in
https://gitlab.com/openconnect/openconnect/-/merge_requests/475.  I've
modified that patch slightly to make it a bit easier to read and more
idiot-proof in the future (while giving the idiot in question a cold hard
stare in the mirror.)

[Incidentally, a misordered `#endif` / `}` pair also made it so that
https://gitlab.com/openconnect/openconnect/-/commit/e2bbc2a1f and later
wouldn't even compile unless `HAVE_ESP` was `#define`d. Probably no one
is building without ESP support… why would they?]

Signed-off-by: Daniel Lenski <dlenski@gmail.com>

gpst.c
@@ -481,11 +481,12 @@ static int gpst_parse_config_xml(struct openconnect_info *vpninfo, xmlNode *xml_
 					     _("Gateway address in config XML (%s) differs from external gateway address (%s).\n"), s, vpninfo->ip_info.gateway_addr);
 #ifdef HAVE_ESP
 			if (esp_magic_af == AF_INET6 && legacy) {
-				/* We ignore the Legacy IP tag if we've already gotten the IPv6 tag. */
+				/* We ignore the Legacy IP tag <gw-address> if we've already gotten the IPv6 tag <gw-address-v6>. */
+			} else {
 				esp_magic_af = legacy ? AF_INET : AF_INET6;
 				inet_pton(esp_magic_af, s, &esp_magic);
-#endif /* HAVE_ESP */
 			}
+#endif /* HAVE_ESP */
 		} else if (!xmlnode_get_val(xml_node, "connected-gw-ip", &s)) {
 			if (vpninfo->ip_info.gateway_addr && strcmp(s, vpninfo->ip_info.gateway_addr))
 				vpn_progress(vpninfo, PRG_DEBUG, _("Config XML <connected-gw-ip> address (%s) differs from external\n"