sailfishos-mirror/openconnect

commit 494edf49e6287daba8aaac7ad0265283eb6ae42d

Add openconnect_set_sni API function and Java setSNI() wrapper
The `--sni` command-line option was added in
https://gitlab.com/openconnect/openconnect/-/merge_requests/297, in order to
experiment with domain fronting.

It seems to have proven useful for many users in Iran (see
https://gitlab.com/openconnect/openconnect/-/merge_requests?label_name[]=Damet%20Garm),
so we should enable front-ends/GUIs to integrate this option.

Signed-off-by: Hossein Khoujani <hosseinkhojany1380@gmail.com>
Signed-off-by: Dimitri Papadopoulos <3350651-DimitriPapadopoulos@users.noreply.gitlab.com>
Signed-off-by: Daniel Lenski <dlenski@gmail.com>

libopenconnect.map.in
@@ -124,6 +124,7 @@ OPENCONNECT_5_8 {
 	openconnect_set_external_browser_callback;
 	openconnect_set_mca_cert;
 	openconnect_set_mca_key_password;
+	openconnect_set_sni;
 	openconnect_set_useragent;
 } OPENCONNECT_5_7;
 

openconnect.h
@@ -575,6 +575,7 @@ int openconnect_set_hostname(struct openconnect_info *, const char *);
 char *openconnect_get_urlpath(struct openconnect_info *);
 int openconnect_set_urlpath(struct openconnect_info *, const char *);
 int openconnect_set_localname(struct openconnect_info *, const char *);
+int openconnect_set_sni(struct openconnect_info *, const char *);
 
 /* Some software tokens, such as HOTP tokens, include a counter which
  * needs to be stored in persistent storage.

java/src/org/infradead/libopenconnect/LibOpenConnect.java
@@ -132,6 +132,7 @@ public abstract class LibOpenConnect {
 	public synchronized native void setPassTOS(boolean isEnabled);
 	public synchronized native int setProxyAuth(String methods);
 	public synchronized native int setHTTPProxy(String proxy);
+	public synchronized native void setSNI(String sni);
 	public synchronized native void setXMLSHA1(String hash);
 	public synchronized native void setHostname(String hostname);
 	public synchronized native void setVersionString(String version);

jni.c
@@ -1459,6 +1459,14 @@ JNIEXPORT void JNICALL Java_org_infradead_libopenconnect_LibOpenConnect_setLocal
 	SET_STRING_END();
 }
 
+JNIEXPORT void JNICALL Java_org_infradead_libopenconnect_LibOpenConnect_setSNI(
+	JNIEnv *jenv, jobject jobj, jstring jarg)
+{
+	SET_STRING_START_VOID()
+	openconnect_set_sni(ctx->vpninfo, arg);
+	SET_STRING_END();
+}
+
 JNIEXPORT void JNICALL Java_org_infradead_libopenconnect_LibOpenConnect_setCAFile(
 	JNIEnv *jenv, jobject jobj, jstring jarg)
 {

library.c
@@ -882,6 +882,15 @@ int openconnect_set_localname(struct openconnect_info *vpninfo,
 	return 0;
 }
 
+int openconnect_set_sni(struct openconnect_info *vpninfo,
+			      const char *sni)
+{
+	UTF8CHECK(sni);
+
+	STRDUP(vpninfo->sni, sni);
+	return 0;
+}
+
 void openconnect_set_xmlsha1(struct openconnect_info *vpninfo,
 			     const char *xmlsha1, int size)
 {

main.c
@@ -1933,7 +1933,7 @@ int main(int argc, char **argv)
 			gai->value = gai->option + (ip - config_arg) + 1;
 			break;
 		case OPT_SNI:
-			vpninfo->sni = keep_config_arg();
+			openconnect_set_sni(vpninfo, config_arg);
 			break;
 		case OPT_NO_DTLS:
 			openconnect_disable_dtls(vpninfo);

commit 6a794cb6f5da49f355c9828b477c7c3e632755af

Move openconnect_set_sni() to API v5.9
We retrospectively added openconnect_set_sni() with the @OPENCONNECT_5_8
symbol version, *long* after API v5.8 was set in stone with the v9.00
release in April 2022.

Fix that by retconning it into a @OPENCONNECT_5_9 version which will be
part of the *next* release.

We have a unit test to prevent us from doing it again, and this commit
is the exception to the general rule that we should *never* commit to
libopenconnect5.symbols except as a side-effect of 'make tag' creating
a new release.

Fixes: 494edf49e628 ("Add openconnect_set_sni API function and Java setSNI() wrapper")
Signed-off-by: David Woodhouse <dwmw2@infradead.org>

libopenconnect.map.in
@@ -124,9 +124,13 @@ OPENCONNECT_5_8 {
 	openconnect_set_external_browser_callback;
 	openconnect_set_mca_cert;
 	openconnect_set_mca_key_password;
-	openconnect_set_sni;
 	openconnect_set_useragent;
 } OPENCONNECT_5_7;
+
+OPENCONNECT_5_9 {
+ global:
+	openconnect_set_sni;
+} OPENCONNECT_5_8;
 
 OPENCONNECT_PRIVATE {
  global: @SYMVER_TIME@ @SYMVER_GETLINE@ @SYMVER_JAVA@ @SYMVER_ASPRINTF@ @SYMVER_VASPRINTF@ @SYMVER_WIN32_STRERROR@ @SYMVER_WIN32_SETENV@

libopenconnect5.symbols
@@ -100,5 +100,4 @@ libopenconnect.so.5 libopenconnect5 #MINVER#
  openconnect_set_external_browser_callback@OPENCONNECT_5_8 9.00
  openconnect_set_mca_cert@OPENCONNECT_5_8 9.00
  openconnect_set_mca_key_password@OPENCONNECT_5_8 9.00
- openconnect_set_sni@OPENCONNECT_5_8 9.00
  openconnect_set_useragent@OPENCONNECT_5_8 9.00

openconnect.h
@@ -33,9 +33,12 @@ extern "C" {
 #endif
 
 #define OPENCONNECT_API_VERSION_MAJOR 5
-#define OPENCONNECT_API_VERSION_MINOR 8
+#define OPENCONNECT_API_VERSION_MINOR 9
 
 /*
+ * API version 5.9:
+ *  - Add openconnect_set_sni()
+ *
  * API version 5.8 (v9.00; 2022-04-29):
  *  - Add openconnect_set_useragent()
  *  - Add openconnect_set_external_browser_callback()