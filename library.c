sailfishos-mirror/openconnect

commit c8df16b70fbcf1bd407ef479f9ad49224eb4bb5f

Use free_pass() in openconnect_vpninfo_free()
Signed-off-by: David Woodhouse <dwmw2@infradead.org>

library.c
@@ -352,11 +352,12 @@ void openconnect_vpninfo_free(struct openconnect_info *vpninfo)
 	free(vpninfo->unique_hostname);
 	free(vpninfo->urlpath);
 	free(vpninfo->redirect_url);
-	free(vpninfo->cookie);
+	free_pass(&vpninfo->cookie);
 	free(vpninfo->proxy_type);
 	free(vpninfo->proxy);
 	free(vpninfo->proxy_user);
-	free(vpninfo->proxy_pass);
+	free_pass(&vpninfo->proxy_pass);
+	free_pass(&vpninfo->cert_password);
 	free(vpninfo->vpnc_script);
 	free(vpninfo->cafile);
 	free(vpninfo->ifname);
@@ -417,7 +418,7 @@ void openconnect_vpninfo_free(struct openconnect_info *vpninfo)
 	free(vpninfo->authgroup);
 #ifdef HAVE_LIBSTOKEN
 	if (vpninfo->stoken_pin)
-		free(vpninfo->stoken_pin);
+		free_pass(&vpninfo->stoken_pin);
 	if (vpninfo->stoken_ctx)
 		stoken_destroy(vpninfo->stoken_ctx);
 #endif
@@ -427,7 +428,7 @@ void openconnect_vpninfo_free(struct openconnect_info *vpninfo)
 			pskc_done(vpninfo->pskc);
 		else
 #endif /* HAVE_LIBPSKC */
-		free(vpninfo->oath_secret);
+		free_pass(&vpninfo->oath_secret);
 	}
 #ifdef HAVE_LIBPCSCLITE
 	release_pcsc_ctx(vpninfo);

commit f5650da1f949cd2406c6cc7cc9d0f8fd8c82b8b8

Fix use of free_pass() on binary oath_secret
The oath_secret is binary data (not NUL-terminated), so free_pass()
which uses strlen() to determine the clear length reads past the
allocation. Use clear_mem() with oath_secret_len instead.

Fixes: c8df16b7 ("Use free_pass() in openconnect_vpninfo_free()")
Signed-off-by: David Woodhouse (Kiro) <dwmw2@infradead.org>

library.c
@@ -909,7 +909,10 @@ void openconnect_vpninfo_free(struct openconnect_info *vpninfo)
 			pskc_done(vpninfo->pskc);
 		else
 #endif /* HAVE_LIBPSKC */
-		free_pass(&vpninfo->oath_secret);
+		{
+			clear_mem(vpninfo->oath_secret, vpninfo->oath_secret_len);
+			free(vpninfo->oath_secret);
+		}
 	}
 #ifdef HAVE_LIBPCSCLITE
 	release_pcsc_ctx(vpninfo);